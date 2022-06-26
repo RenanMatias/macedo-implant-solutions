9acf5afca78f1c21860761b4255d9a8ab106ebde
Renan Matias <rnmatias@hotmail.com>
2022-06-26 15:15:30 -0300

    Created Template Dashboard and add Nav logo and list

--- apps/core/views/dashboard.py
+++ apps/core/views/dashboard.py
@@ -2,8 +2,6 @@ from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.views.generic.base import TemplateView
 
-# @login_required(login_url='login', redirect_field_name='next')
-
 
 @method_decorator(
     login_required(
@@ -14,3 +12,12 @@ from django.views.generic.base import TemplateView
 )
 class DashboardView(TemplateView):
     template_name = 'core/dashboard.html'
+
+    def get_context_data(self, *args, **kwargs):
+        ctx = super().get_context_data(*args, **kwargs)
+        ctx.update(
+            {
+                'display_nav': 'dashboard'
+            }
+        )
+        return ctx
