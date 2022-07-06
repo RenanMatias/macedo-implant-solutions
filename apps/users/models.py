3cba045f1d302fcb7685b002079bed467ba5c42a
Renan Matias <rnmatias@hotmail.com>
2022-07-06 18:40:12 -0300

    Display user picture or first letter of first name and last name

--- apps/users/models.py
+++ apps/users/models.py
@@ -8,3 +8,9 @@ class User(AbstractUser):
         blank=True,
         default=''
     )
+
+    def first_letter_first_name(self):
+        return self.first_name[0]
+
+    def first_letter_last_name(self):
+        return self.last_name[0]
