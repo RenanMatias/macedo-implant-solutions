5ef6a80e4e4901ccb6a4ad7988752a672011810a
Renan Matias <rnmatias@hotmail.com>
2022-08-19 21:53:45 -0300

    Included dynamic button icon

--- apps/core/views/profile.py
+++ apps/core/views/profile.py
@@ -26,6 +26,7 @@ class ProfileView(UpdateView):
 
         ctx.update({
             'button_title': 'Atualizar Dados',
+            'button_icon': 'bi bi-check2',
         })
 
         return ctx
@@ -36,6 +37,7 @@ class ProfileView(UpdateView):
         user.user = self.request.user
 
         user.save()
+        update_session_auth_hash
 
         success(self.request, 'Dados atualizados com sucesso.')
         return redirect(reverse('core:profile', args=[self.object.pk]))
